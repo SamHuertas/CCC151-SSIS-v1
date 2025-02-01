1cac364d363fa51b985af47bea0719b29a0a7086
Sam Alexis Huertas <156677818+SamHuertas@users.noreply.github.com>
2025-02-01 12:36:19 +0800

    read and write CSV

--- src.java
+++ src.java
@@ -1,18 +1,28 @@
 import java.io.*;
 import java.util.*;
 
+
 public class src {
     public static void main(String[] args){
         String path = "/Users/SAM/Desktop/SSIS/student.csv";
         String line = "";
+        
 
         try{
-        BufferedReader br = new BufferedReader(new FileReader(path));
-        while((line = br.readLine()) != null){
-            String values[] = line.split(",");
-            System.out.println(values[1]);
-        }
+            BufferedReader br = new BufferedReader(new FileReader(path));
+            while((line = br.readLine()) != null){
+                System.out.println(line);
+            }
+            br.close();
 
+            Scanner newinpt = new Scanner(System.in);
+            System.out.println("Enter new data to append:");
+            String x = newinpt.nextLine();
+
+            BufferedWriter inptdata = new BufferedWriter(new FileWriter(path, true));
+            inptdata.newLine();
+            inptdata.write(x);
+            inptdata.close();
 
         } catch (FileNotFoundException e){
             e.printStackTrace();
